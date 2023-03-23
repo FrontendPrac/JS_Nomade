2d99ad84e14fce11ba3afc9f583fb71a5187327b
Kidongg <dlrlehd7@daum.net>
2023-03-23 17:15:33 +0900

    3.22 ~ 23 업데이트

--- README.md
+++ README.md
@@ -9,3 +9,17 @@
 - null : 자연적으로 발생하지 않는 타입, 값이 없다는 것을 알리기 위해 사용한다.
 - undefined : 자연적으로 발생하는 타입, 값이 없음을 의미한다.
 - const로 객체를 만들더라도 객체의 속성값은 변경이 가능하다.
+
+# 자바스크립트(3.22)
+
+- 객체안에서도 함수를 정의할 수 있다. key값에 함수 이름, value값에 함수를 정의하면 된다.
+
+# 자바스크립트(3.23)
+
+- 자바스크립트로 HTML 요소를 제어할 수 있다.
+- 제어 방법 : HTML 태그를 읽어온다. 해당 태그의 속성을 제어한다.
+- querySelector, querySelectorAll은 CSS 셀레팅 문법 사용이 가능하다.
+- console.dir : HTML 태그의 속성을 객체형태로 확인할 수 있다.
+- document, window
+- 마우스 이벤트 : onmouseenter, onmouseleave, onmousedown
+- 윈도우 이벤트 : resize, oncopy, onoffline, ononline
--- app.js
+++ app.js
@@ -1,1 +1,56 @@
-alert("자바스크립트 세계에 오신걸 환영합니다!!");
+const caculation = {
+  plus: function (a, b) {
+    console.log(a + b);
+  },
+  minus: function (a, b) {
+    console.log(a - b);
+  },
+};
+
+// caculation.plus(10, 5);
+// caculation.minus(10, 5);
+
+document.title = "Good";
+
+const title = document.querySelector("#title");
+console.log("title: ", title);
+console.log("title.id: ", title.id);
+console.log("title.textContent: ", title.textContent);
+console.log("title.hidden: ", title.hidden);
+console.log("title.autofocus: ", title.autofocus);
+
+console.dir(title);
+
+title.innerText = "Click me!"; //? 텍스트
+title.style.color = "blue"; //? 스타일
+
+function handleTitleClick() {
+  console.log("title was clicked");
+  title.style.color = "red";
+}
+
+title.addEventListener("click", handleTitleClick); //? 이벤트
+
+function handleMouseEnter() {
+  title.innerText = "Hello!";
+}
+
+title.addEventListener("mouseenter", handleMouseEnter); //? 이벤트
+
+function handleMouseLeave() {
+  title.innerText = "Bye Bye!";
+}
+
+title.addEventListener("mouseleave", handleMouseLeave); //? 이벤트
+
+function handleResize() {
+  document.body.style.backgroundColor = "#FF7E9D";
+}
+
+window.addEventListener("resize", handleResize);
+
+function handleMouseDown() {
+  title.style.color = "orange";
+}
+
+window.addEventListener("mousedown", handleMouseDown);
