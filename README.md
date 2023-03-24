303511e71c52af77e957d95753cc1772b43f2f70
Kidongg <dlrlehd7@daum.net>
2023-03-24 22:39:34 +0900

    3.24 업데이트

--- README.md
+++ README.md
@@ -23,3 +23,9 @@
 - document, window
 - 마우스 이벤트 : onmouseenter, onmouseleave, onmousedown
 - 윈도우 이벤트 : resize, oncopy, onoffline, ononline
+
+# 자바스크립트(3.24)
+
+- 사용자와 상호작용하는 방법 : HTML 요소를 찾는다. event를 listen한다. event에 반응한다.
+- 보편적으로 CSS는 style을 JS는 animation을 제어하는 용도로 사용한다.
+- classList.toggle : 기존 class명 뒤에 새로운 class명을 추가하는 방식으로 토글링한다.
--- app.js
+++ app.js
@@ -29,24 +29,28 @@ function handleTitleClick() {
   title.style.color = "red";
 }
 
+//! 클식 시 텍스트 변경하기
 title.addEventListener("click", handleTitleClick); //? 이벤트
 
 function handleMouseEnter() {
   title.innerText = "Hello!";
 }
 
+//! 마우스 입장 시 색상 변경하기
 title.addEventListener("mouseenter", handleMouseEnter); //? 이벤트
 
 function handleMouseLeave() {
   title.innerText = "Bye Bye!";
 }
 
+//! 마우스 이탈 시 색상 변경하기
 title.addEventListener("mouseleave", handleMouseLeave); //? 이벤트
 
 function handleResize() {
   document.body.style.backgroundColor = "#FF7E9D";
 }
 
+//! resize 시 색상 변경하기
 window.addEventListener("resize", handleResize);
 
 function handleMouseDown() {
@@ -54,3 +58,25 @@ function handleMouseDown() {
 }
 
 window.addEventListener("mousedown", handleMouseDown);
+
+//! 클릭 시 색상 변경하기
+const content = document.querySelector("#content");
+
+function handleClick() {
+  if (content.style.color === "red") {
+    content.style.color = "black";
+  } else {
+    content.style.color = "red";
+  }
+}
+
+content.addEventListener("click", handleClick);
+
+//! 클릭 시 색상 변경하기(CSS in JavaScript)
+const h3 = document.querySelector("h3");
+
+function handleClick2() {
+  h3.classList.toggle("active2");
+}
+
+h3.addEventListener("click", handleClick2);
