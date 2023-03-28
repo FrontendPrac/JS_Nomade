01db841c2a4b78cb840ee8d18a0a0db3ea3690c0
Kidongg <dlrlehd7@daum.net>
2023-03-28 20:18:16 +0900

    3.28 업데이트

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
-//! 버튼을 클릭 시 입력값 받아오기
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
+const maxNumElement = document.querySelector("#max-number");
+const guessNumElement = document.querySelector("#guess-number");
 
-const USERNAME = "username";
-const HIDDEN = "hidden";
+const submitBtn = document.querySelector("#submit-btn");
 
-function onSubmitHandler(event) {
+const machine = document.querySelector("#machine");
+
+const resultWin = document.querySelector("#result-win");
+const resultLose = document.querySelector("#result-lose");
+
+function toggleResult(event) {
   event.preventDefault();
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME, username);
-  loginForm.classList.add(HIDDEN);
-  link.classList.remove(HIDDEN);
-  loginInput.value = "";
+
+  if (maxNumElement.value === "" || guessNumElement.value === "") {
+    alert("Input another Number!");
+    return;
+  }
+
+  const maxNum = parseInt(maxNumElement.value);
+  const guessNum = parseInt(guessNumElement.value);
+
+  if (maxNum < 0 || guessNum < 0 || guessNum > maxNum) {
+    alert("Chose another Number!");
+    return;
+  }
+
+  const machineNum = Math.ceil(Math.random() * maxNum);
+
+  machine.innerText = `You chose: ${guessNum}, the machine chose: ${machineNum}.`;
+  if (guessNum === machineNum) {
+    resultWin.innerText = "You won!";
+    resultWin.classList.remove("hidden");
+    resultLose.classList.add("hidden");
+  } else {
+    resultLose.innerText = "You lost!";
+  }
 }
 
-//! 링크 클릭 시 페이지 이동 막기
-const link = document.querySelector("a");
-
-function onClickHandler(event) {
-  event.preventDefault();
-  localStorage.removeItem(USERNAME);
-  loginForm.classList.remove(HIDDEN);
-  link.classList.add(HIDDEN);
-}
-
-//! 로컬 스토리지 유무에 따라 토클링하기
-const saveUsername = localStorage.getItem(USERNAME);
-console.log(saveUsername);
-
-if (saveUsername === null) {
-  loginForm.classList.remove(HIDDEN);
-  loginForm.addEventListener("submit", onSubmitHandler);
-} else {
-  link.classList.remove(HIDDEN);
-  link.addEventListener("click", onClickHandler);
-}
+submitBtn.addEventListener("click", toggleResult);
