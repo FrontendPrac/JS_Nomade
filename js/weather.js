4d8d1c3d48775aeb95aad24ae9891f21f3b9d9ca
Kidongg <dlrlehd7@daum.net>
2023-04-06 22:53:19 +0900

    4.6 업데이트 : WEATHER

--- js/weather.js
+++ js/weather.js
@@ -1,8 +1,23 @@
+// API KEY
+const API_KEY = "7c85994319253cd946d68c5fbb295cf4";
+
 // 성공 콜백함수
 function onGeoSucces(position) {
   const lat = position.coords.latitude;
-  const lng = position.coords.longitude;
-  console.log("You live in", lat, lng);
+  const lon = position.coords.longitude;
+  // console.log("You live in", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const city = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:nth-child(2n)");
+      const temp = document.querySelector("#weather span:last-child");
+
+      city.innerText = data.name;
+      weather.innerText = data.weather[0].main;
+      temp.innerText = data.main.temp;
+    });
 }
 
 // 실패 콜백함수
